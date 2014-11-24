a941cb43e340413d4ed685d8f3baeae2b17c7416
keydet89 <keydet89@yahoo.com>
2014-11-24 15:47:59 -0500

    updated regin.pl
    updated regin.pl again

--- plugins/regin.pl
+++ plugins/regin.pl
@@ -53,43 +53,22 @@ sub pluginmain {
 	if ($key = $root_key->get_subkey($key_path)) {
 		$current = $key->get_value("Current")->get_data();
 		$ccs = "ControlSet00".$current;
-		
-# Symantec Whitepaper: Stage 2		
-# Kaspersky calls this Stage 1
-		$key_path = $ccs."\\Control\\Class\\{4F20E605-9452-4787-B793-D0204917CA58}";
-		if ($key = $root_key->get_subkey($key_path)) {
-			::rptMsg("Possible Regin Stage 2 found in ".$key_path);
-		}
 
-# Symantec Whitepaper: Stage 3		
-		$key_path = $ccs."\\Control\\Class\\{4F20E605-9452-4787-B793-D0204917CA5A}";
-		if ($key = $root_key->get_subkey($key_path)) {
-			::rptMsg("Possible Regin Stage 3 found in ".$key_path);
-		}
-
-# Kaspersky Whitepaper: Stage 2, 32-bit	
-		$key_path = $ccs."\\Control\\Class\\{39399744-44FC-AD65-474B-E4DDF-8C7FB97}";
-		if ($key = $root_key->get_subkey($key_path)) {
-			::rptMsg("Possible Regin Stage 2 found in ".$key_path);
-		}
-
-# Kaspersky Whitepaper: Stage 2, 32-bit			
-		$key_path = $ccs."\\Control\\Class\\{3F90B1B4-58E2-251E-6FFE-4D38C5631A04}";
-		if ($key = $root_key->get_subkey($key_path)) {
-			::rptMsg("Possible Regin Stage 2 found in ".$key_path);
-		}
-		
-# Kaspersky Whitepaper: Stage 3 (?)		
-		$key_path = $ccs."\\Control\\Class\\{9B9A8ADB-8864-4BC4-8AD5-B17DFDBB9F58}";
-		if ($key = $root_key->get_subkey($key_path)) {
-			::rptMsg("Possible Regin Stage 2 found in ".$key_path);
-		}		
-				
-# Symantec Whitepaper: Stage 2, version 2 only (?)		
-		$key_path = $ccs."\\Control\\RestoreList\\VideoBase";
-		if ($key = $root_key->get_subkey($key_path)) {
-			::rptMsg("Possible Regin Stage 2 found in ".$key_path);
+		my %stages = ("\\Control\\Class\\{4F20E605-9452-4787-B793-D0204917CA58}" => "Symantec stage 2/Kaspersky stage 1",
+              		"\\Control\\Class\\{4F20E605-9452-4787-B793-D0204917CA5A}" => "Symantec stage 3",
+              		"\\Control\\Class\\{39399744-44FC-AD65-474B-E4DDF-8C7FB97}" => "Kaspersky stage 2, 32-bit",
+              		"\\Control\\Class\\{3F90B1B4-58E2-251E-6FFE-4D38C5631A04}" => "Kaspersky stage 2, 32-bit",
+              		"\\Control\\Class\\{9B9A8ADB-8864-4BC4-8AD5-B17DFDBB9F58}" => "Kaspersky stage 3 (?)",
+              		"\\Control\\RestoreList\\VideoBase" => "Symantec stage 2, v.2.0");
+              
+		foreach my $i (keys %stages) {
+			$key_path = $ccs.$i;
+			if ($key = $root_key->get_subkey($key_path)) {
+				::rptMsg("Possible Regin infection found in ".$i);
+				::rptMsg($stages{$i});
+			}
 		}
 	}
+		
 }
 1;
